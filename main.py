9d0393ce6af2cf45c28fed885ec735675e6a0fd8
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2025-09-12 15:50:53 +0530

    updated main.py to use cli prompts

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import sys
+
+
+
 def main():
-    print("Hello from coding-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client=genai.Client(api_key=api_key)
+
+    # sys.argv variable is a list of strings representing the command-line arguments passed to the script. First element is the script name, rest are the arguments passed to the script.
+
+    if len(sys.argv)<2:
+        print("I didn't receieve a prompt!")
+        sys.exit(1)
+    verbose_flag=False
+    if(len(sys.argv)==3 and sys.argv[2]=='-v'):
+        verbose_flag=True
+    prompt=sys.argv[1]
+
+    messages=[
+        types.Content(
+            role='user',
+            parts=[
+                types.Part(text=prompt)
+            ]
+        )
+    ]
 
 
-if __name__ == "__main__":
-    main()
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=messages
+    )
+
+    print(response.text)
+    if verbose_flag:
+        print("User Prompt:",prompt)
+        print(messages)
+        print(sys.argv[1])
+        print("Prompt tokens:",response.usage_metadata.prompt_token_count)
+        print("Response tokens:",response.usage_metadata.candidates_token_count)
+
+main()
